42edc2a24ccda7a5d4eb38f53db104349ee9a0a3
Reeta <retdream@gmail.com>
2017-10-14 13:27:59 +1100

    Bot displays image

--- server/app.js
+++ server/app.js
@@ -1,32 +1,56 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+let restify = require('restify');
+let builder = require('botbuilder');
+let axios = require('axios');
 
 // Setup Restify Server
-var server = restify.createServer();
+let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
+let connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+let baseURL = "https://api.unsplash.com/";
+let appID = "4df1f42354643ea43ff6e9475ed6ba4f2984f470baadac55b85df0a37a1e4bbd";
+
+let getRandomFoto = function(session){
+  axios.get(`${baseURL}photos/random`, {
+    params: {
+      client_id: appID,
+      w: 1280,
+      query: 'landscape'
+    }
+  }).then(function(res){
+    let card = createHeroCard(session, res.data.urls.regular);
+    console.log(res.data.urls.regular);
+    let msg = new builder.Message(session).addAttachment(card);
+    session.send(msg);
+    // $("#random_image").attr('src', res.urls.regular);
+  })
+};
+
+
+const image = 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg';
+
+function createHeroCard(session, url) {
+  return new builder.HeroCard(session)
+    .images([
+      builder.CardImage.create(session, url)
+    ])
+  }
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-    return new builder.HeroCard(session)
-    .title('BotFramework Hero Card')
-    .subtitle('Your bots — wherever your users are talking')
-    .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
-    .images([
-        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-    ])
-    .buttons([
-        builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework', 'Get Started')
-    ]);
+let bot = new builder.UniversalBot(connector, function (session) {
+  let msg = new builder.Message(session);
+  // let card = createHeroCard(session, url);
+  getRandomFoto(session);
+   // attach the card to the reply message
+
+
 });
